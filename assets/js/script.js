62d2a4ac3fa6443b5b66ecbeeb66b531e6c30b16
JGibson90 <j_gibson6@hotmail.com>
2021-05-06 20:31:20 +0000

    feature: added bootstrap form for better responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//Jquery 
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+    $('#slideshow > div:first')
+    .fadeOut(4000)
+    .next()
+    .fadeIn(4000)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
